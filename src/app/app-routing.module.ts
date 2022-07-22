8703504a38e4f4b4b92ed0adf4c2e7f1ba5ead27
Caio Alexandre <44368299+caioalexandre6@users.noreply.github.com>
2022-07-21 23:13:10 -0300

    list-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
+import { CharacterDetailsComponent } from './character-details/character-details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharacterListComponent } from './character-list/character-list.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'character-list',
     component: CharacterListComponent
   },
+  { path: 'character-details', component: CharacterDetailsComponent },
+  { path: '',   redirectTo: '/character-list', pathMatch: 'full' },
+/*   { path: '**', component: PageNotFoundComponent }, */
 ];
 
 @NgModule({
--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Injectable, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ServiceMarvel } from '../services/serviceMarvel';
 
 @Component({
@@ -11,11 +12,14 @@ import { ServiceMarvel } from '../services/serviceMarvel';
 })
 export class CharacterListComponent implements OnInit {
   public searchInput: any;
+  public listByGrid: boolean;
   public listCharacter: Array<any>;
   public listCharacterAux: Array<any>;
-  constructor(private api: ServiceMarvel) {
+
+  constructor(private api: ServiceMarvel, private router: Router) {
     this.listCharacter = new Array<any>();
     this.listCharacterAux = new Array<any>();
+    this.listByGrid = false;
   }
 
   pesquisar() {
@@ -31,7 +35,7 @@ export class CharacterListComponent implements OnInit {
     });
   }
 
-  async id(id: any){
+  async id(id: any) {
     console.log('CLICOU AI');
     await this.api.getCharacterById(id).then(async (resp: any) => {
       this.listCharacter = resp.data.results;
@@ -39,10 +43,23 @@ export class CharacterListComponent implements OnInit {
     });
   }
 
-  async ngOnInit(): Promise<void> {
+  listBy() {
+    if (this.listByGrid === false) {
+      this.listByGrid = true;
+    } else {
+      this.listByGrid = false;
+    }
+  }
+
+  navigateByRoute(character: any){
+    this.router.navigate(['/character-details'], character);
+  }
+
+  async ngOnInit() {
     await this.api.getCharacters().then(async (resp: any) => {
       this.listCharacter = resp.data.results;
       this.listCharacterAux = this.listCharacter;
+      console.log('list', this.listCharacter);
     });
   }
 }
